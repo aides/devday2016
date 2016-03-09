b05a126dedd420a5697716efcc13713ea4ea8f15
Mikhail Kuleshov <aidesune@gmail.com>
2016-03-09 23:40:10 +0300

    Fixed tests

--- spec/indexController.spec.js
+++ spec/indexController.spec.js
@@ -4,14 +4,14 @@ const app = require('../src/app');
 describe('IndexController', function() {
     it('should return \'Hello World!\'', function(done) {
         request(app)
-        .get('/')
+        .get('/helloworld')
         .expect(200)
         .end((err, res) => {
             if (err) {
                 console.log(`ERROR ${JSON.stringify(err)}`);
                 done.fail();
             } else {
-                expect(res.text).toEqual('Hello World 123!');
+                expect(res.text).toEqual('Hello World!');
                 done();
             }
         });
