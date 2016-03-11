7df671b6796f96a3d00eaba046b6966caddb961a
Mikhail Kuleshov <aidesune@gmail.com>
2016-03-11 21:24:18 +0300

    Fixed test

--- spec/indexController.spec.js
+++ spec/indexController.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('IndexController', function() {
-    it('should return \'Hello World!\'', function(done) {
+    it('should return \'Hello World123!\'', function(done) {
         request(app)
         .get('/helloworld')
         .expect(200)
