9dcf85c5f31f94783f4f095c6ef031fe20aaee82
Mikhail Kuleshov <aidesune@gmail.com>
2016-03-11 21:27:10 +0300

    Fixed the test

--- spec/indexController.spec.js
+++ spec/indexController.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('IndexController', function() {
-    it('should return \'Hello World123!\'', function(done) {
+    it('should return \'Hello World!\'', function(done) {
         request(app)
         .get('/helloworld')
         .expect(200)
@@ -11,7 +11,8 @@ describe('IndexController', function() {
                 console.log(`ERROR ${JSON.stringify(err)}`);
                 done.fail();
             } else {
-                expect(res.text).toEqual('Hello World!');
+                // ASSERT
+                expect(res.text).toEqual('Hello World123!');
                 done();
             }
         });
